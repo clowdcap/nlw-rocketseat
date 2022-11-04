e4ed69adb7a622b2c4082170baa4d361f8ecc054
José Marinho <clowdcap@hotmail.com>
2022-11-04 16:07:21 -0300

    update post pool server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,8 @@
 import Fastify, { fastify } from 'fastify'
 import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
     log: ['query'],
@@ -20,7 +22,7 @@ async function bootstrap() {
 
     // http://localhost:5000
 
-    // Criando rotas
+    // Criando rotas - Get
     fastify.get('/pools/count', async ()=> {
 
         // const pools = await prisma.pool.findMany({
@@ -35,6 +37,38 @@ async function bootstrap() {
         const count = await prisma.pool.count()
         return { count }
     })
+
+    fastify.get('/users/count', async ()=> {
+        const count = await prisma.user.count()
+        return { count }
+    })
+
+    fastify.get('/guesses/count', async ()=> {
+        const count = await prisma.guess.count()
+        return { count }
+    })
+
+    // Rotas Geral - Post
+    fastify.post('/pools', async (request, reply) => {
+        const createPoolBody = z.object({
+            title: z.string(),
+        })
+        const { title } = createPoolBody.parse(request.body)
+
+        const generate = new ShortUniqueId({length: 6})
+        const code = String(generate()).toUpperCase()
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code,
+            }
+        })
+        return reply.status(201).send({ code })
+        // return { title }
+    })
+
+    
 
     // Ligando servidor
     await fastify.listen({ port: 5000, host: '0.0.0.0' })
